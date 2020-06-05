aa0d14ddf8ea696fdd9dd385a66acb1650d54a27
Dirk Uys <dirkcuys@gmail.com>
2020-06-05 15:10:33 +0200

    Code for creating pads

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const yargs = require('yargs');
 const zmq = require("zeromq");
-/* Do a stress test of an etherpad-lite server
- * Use zeromq to coordinate a bunch of workers to participate on the pads
- * What should the workers do?
+const https = require("https");
+const querystring = require('querystring');
+/* Do a stress test of an etherpad-lite server. The idea is to create a realistic workload on the server using distributed workers
+ * At the end, I'd like to be able to measure any increase, decrease in performance
+ * Workers
+ *  - Start up headless chrome
+ *  - Wait for pad URL
  *  - Connect to the pad
- *  - Post test to the pad
- * What should a worker measure
- *  - Time from navigation until page is ready
- *  - Total amount of text contributed to etherpads
- * What does the server do
- *  - Wait for enough workers to connect
+ *  - Interact with the pad for some time
+ *  - Report results
+ *     - Time from navigation until page is ready
+ *     - Total amount of text contributed to etherpads
+ * Server
+ *  - Create 1 or more etherpads to use for testing
+ *  - When a worker connects, assign it to a pad
  *  - Send URLs that should be connected to
  *  - Gather metrics taken
+ *  - Keep track of how many workers are active
  */
 
 const argv = yargs
@@ -27,6 +33,12 @@ const argv = yargs
     description: 'Etherpad server',
     type: 'string',
   })
+  .option('etherpad-api-key', {
+    alias: 'K',
+    demandOption: true,
+    description: 'Etherpad API KEY',
+    type: 'string',
+  })
   .option('required-workers', {
     alias: 'w',
     type: 'int',
@@ -37,13 +49,39 @@ const argv = yargs
   .argv;
 
 
-async function testEnv(padCount){
-  // create padCount new pads and return an array
-
+async function createPad(serverUrl, apikey, padID, text){
+  // create a new etherpad
+  const postData = querystring.stringify({text});
+  const url = `${serverUrl}/api/1/createPad?apikey=${apikey}&padID=${padID}&text=${postData}`;
+  //console.log(url);
+  const response = await new Promise( (resolve, reject) => {
+    console.log(`create pad ${padID}`);
+    const req = https.request(url, {
+      //method: 'POST',
+      //headers: {
+      //  'Content-Type': 'application/x-www-form-urlencoded',
+      //  'Content-Length': Buffer.byteLength(postData)
+      //}
+    }, res => {
+      let body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on('end', () => {
+        resolve(body);
+      });
+    });
+    //req.write(postData);
+    req.end();
+  });
+  console.log(response);
 }
 
 async function runServer(bindAddress, etherpadServer){
+  const apikey = argv['etherpad-api-key'];
   const requiredWorkers = argv['required-workers'];
+  await createPad(etherpadServer, apikey, 'test112233', 'This is a new pad to be used for testing');
+
   let ccHost = `tcp://${bindAddress}`;
   // socket used to coordinate
   const ccSock = new zmq.Pull;
@@ -60,7 +98,7 @@ async function runServer(bindAddress, etherpadServer){
   for await (const [msg] of ccSock) {
     var topic = msg.toString();
     console.log("New worker connected: %s", topic);
-    let url = `${etherpadServer}/p/testpad`;
+    let url = `${etherpadServer}/p/test112233`;
     workers.set(topic, url);
     await sock.send([topic, JSON.stringify({url})]);
     if (workers.size >= requiredWorkers) {
